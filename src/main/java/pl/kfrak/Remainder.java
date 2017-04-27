92e1bd4f6169d179608885b9db6400d5d6b7b800
michal.papuzynski <michal.papuzynski@gmail.com>
2017-04-27 18:03:29 +0200

    // //         ,---._ //       .-- -.' \    ,---,                     ,---, //       |    |   :  '  .' \            ,---.  '  .' \ //       :    ;   | /  ;    '.         /__./| /  ;    '. //       :        |:  :       \   ,---.;  ; |:  :       \ //       |    :   ::  |   /\   \ /___/ \  | |:  |   /\   \ //       :         |  :  ' ;.   :\   ;  \ ' ||  :  ' ;.   : //       |    ;   ||  |  ;/  \   \\   \  \: ||  |  ;/  \   \ //   ___ l         '  :  | \  \ ,' ;   \  ' .'  :  | \  \ ,' // /    /\    J   :|  |  '  '--'    \   \   '|  |  '  '--' ///  ../  `..-    ,|  :  :           \   `  ;|  :  : //\    \         ; |  | ,'            :   \ ||  | ,' // \    \      ,'  `--''               '---" `--'' //  "---....--'

---
+++ src/main/java/pl/kfrak/Remainder.java
@@ -0,0 +1,12 @@
+package pl.kfrak;
+
+import java.util.List;
+
+/**
+ * Created by RENT on 2017-04-27.
+ */
+public class Remainder {
+        public List<Integer> getChangeNominals(int amount) {
+            return null;
+        }
+}
